c94f0329b1e6ef02e3dc5fa7dcebb06304d32d50
Rodrigo Barbosa <rodrigo.luiz.barbosa0@gmail.com>
2017-08-14 22:35:10 -0300

    First version, just started the code

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ x = list()
 y = list()
 t = 1
 hour_list = list()
+plt.figure()
 while True:
     hora = time.strftime("%H:%M:%S", time.localtime())
     hour_list.append(hora)
-    print hour_list
     x.append(t)
     y.append(t)
     t = t + 1
     plt.xticks(x, hour_list)
     plt.scatter(x, y)
-    print x
-    time.sleep(1)
+    plt.pause(1)
