843924db50e0014338c84aa4f965bb0c8f506b71
Rodrigo Barbosa <rodrigo.luiz.barbosa0@gmail.com>
2017-08-14 22:59:40 -0300

    First version, just started the code

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ y = list()
 t = 1
 hour_list = list()
 plt.figure()
+plt.xlabel('Horario')
+plt.ylabel('Temperatura')
 while True:
     hora = time.strftime("%H:%M:%S", time.localtime())
     hour_list.append(hora)
     x.append(t)
-    y.append(t)
+    y.append(np.random.random())
     t = t + 1
     plt.xticks(x, hour_list)
-    plt.scatter(x, y)
+    plt.plot(x, y)
     plt.pause(1)
